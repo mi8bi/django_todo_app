f6452d26220227ae5f9e0461c363d62091c031b2
mi8bi <151357186+mi8bi@users.noreply.github.com>
2025-06-22 07:51:57 +0900

    static files

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -150,7 +150,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # アプリケーションごとの静的ファイルディレクトリ
 STATICFILES_DIRS = [
